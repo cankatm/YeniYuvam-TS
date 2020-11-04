45f8c1964d1176a084c78530d1fd872b60f54a85
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-04 17:32:28 +0300

    Heplers :: colors added

---
+++ src/helpers/colors.tsx
@@ -0,0 +1,7 @@
+export const transparent = 'transparent';
+export const white = '#fff';
+export const bluePastel = '#6998F7';
+export const blueDark = '#26395F';
+export const greenPastel = '#6FCF97';
+export const orangePastel = '#F6CC71';
+export const greyLight = '#979797';
