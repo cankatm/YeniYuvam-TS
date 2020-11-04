e5c98faf1850d3c2d918e3528d555671a6e59bbb
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-04 17:33:09 +0300

    Images :: new images

---
+++ src/helpers/images.tsx
@@ -0,0 +1,21 @@
+import likeHoverIcon from '@src/images/like_hover_icon.png';
+import likeNormalIcon from '@src/images/like_normal_icon.png';
+import paperHoverIcon from '@src/images/paper_hover_icon.png';
+import paperNormalIcon from '@src/images/paper_normal_icon.png';
+import personHoverIcon from '@src/images/person_hover_icon.png';
+import personNormalIcon from '@src/images/person_normal_icon.png';
+import searchHoverIcon from '@src/images/search_hover_icon.png';
+import searchNormalIcon from '@src/images/search_normal_icon.png';
+import addIcon from '@src/images/add_icon.png';
+
+export {
+  likeHoverIcon,
+  likeNormalIcon,
+  paperHoverIcon,
+  paperNormalIcon,
+  personHoverIcon,
+  personNormalIcon,
+  searchHoverIcon,
+  searchNormalIcon,
+  addIcon,
+};
