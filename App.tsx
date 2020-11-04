cf2405f8b3c8ec0e9cfe1c88745b967a0750e6ff
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-04 17:30:42 +0300

    [CORE] :: init

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { MainNavigator } from '@helpers/navigationStructure';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SafeAreaView style={styles.container}>
+        <MainNavigator />
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
@@ -15,7 +18,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,44 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          "root": ["./"],
+          "alias": {
+            "@components": "./src/components",
+            "@src": "./src",
+            "@helpers": "./src/helpers",
+            "@screens": "./src/screens",
+            "@utils": "./src/utils",
+            "@assets": "./assets",
+          }
+        },
+      ],
+    ],
+    //   env: {
+    //     production: {
+    //       plugins: [
+    //         [
+    //           'inline-dotenv',
+    //           {
+    //             path: '.env',
+    //           },
+    //         ],
+    //       ],
+    //     },
+    //     development: {
+    //       plugins: [
+    //         [
+    //           'inline-dotenv',
+    //           {
+    //             path: '.env',
+    //           },
+    //         ],
+    //       ],
+    //     },
+    //   },
   };
 };
