fcbe73e2cc16341a7dfc18e91789684cd465d058
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-04 17:33:47 +0300

    Tests :: new tests added

---
+++ src/tests/src/components/buttons/BackButton.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { BackButton } from '@components/buttons';
+import { interpolate } from 'react-native-reanimated';
+
+describe('<BackButton />', () => {
+  it('given empty BackButton, programmer can add an item to it');
+});
